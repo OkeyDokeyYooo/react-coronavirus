43b89c19ece9bb083fea2eb9f99da35ff3eee26a
OkeyDokeyYooo <873262667a@gmail.com>
2020-02-26 23:58:10 -0800

    Nav Bar done

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import {NavbarBrand, Navbar, NavbarToggler,Collapse, NavItem,Nav } from 'reactstrap';
+import {Link} from 'react-router-dom';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      isOpen: false,
+    }
+  }
+
+  render(){
+    return (
+      <div className="App">
+          <Navbar className="border-bottom" expand="md" light>
+            <NavbarBrand > COVID-19 </NavbarBrand>
+            <NavbarToggler onClick={() => this.setState({isOpen: !this.state.isOpen}) } className="border-0"  navbar/>
+            <Collapse isOpen={this.state.isOpen} color="black" navbar>
+            <Nav className="ml-auto" navbar>
+              <NavItem>
+                <Link className="nav-link" to="/"> Data </Link>
+              </NavItem>
+              <NavItem>
+                <Link className="nav-link" to="/News"> News </Link>
+              </NavItem>
+            </Nav>
+            </Collapse>
+          </Navbar>
+      </div>
+    )
+  }
 }
 
 export default App;
