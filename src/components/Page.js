6959882f2dd1210de461d59c283663b32428ba02
OkeyDokeyYooo <873262667a@gmail.com>
2020-03-20 09:19:01 -0700

    changed css

--- src/components/Page.js
+++ src/components/Page.js
@@ -1,6 +1,5 @@
 import React, {Component} from 'react';
 import { Route } from 'react-router-dom';
-import {Row, Container} from 'reactstrap';
 import Axios from 'axios';
 import moment from 'moment-timezone';
 import NoSsr from '@material-ui/core/NoSsr';
@@ -9,7 +8,7 @@ import News   from './News/News';
 import Map    from './Map';
 import Chart from './Chart';
 import SummaryBoard from './SummaryBoard/SummaryBoard'
-import { Grid } from '@material-ui/core';
+import TempBoard from './TempBoard.js'
 
 import './Page.css'
 
@@ -35,36 +34,37 @@ class Page extends Component {
         })
     }
 
-    componentDidMount(){
-        const today = moment.utc().format('YYYY-MM-DD')
-        const request = "http://18.218.58.203:8000/entries/" + today
+    // componentDidMount(){
+    //     const today = moment.utc().format('YYYY-MM-DD')
+    //     const request = "http://18.218.58.203:8000/entries/" + today
 
-        Axios.get(request)
-            .then(res => {
-                if (res.data != null ) {
-                    const localData = res.data.trk;
-                    let summary = localData.pop()
-                    let diff = res.data.diff
-                    this.setState({
-                        data: localData,
-                        summary: summary,
-                        diff: diff
-                    })
-                }
-            })
-    }
+    //     Axios.get(request)
+    //         .then(res => {
+    //             if (res.data != null ) {
+    //                 const localData = res.data.trk;
+    //                 let summary = localData.pop()
+    //                 let diff = res.data.diff
+    //                 this.setState({
+    //                     data: localData,
+    //                     summary: summary,
+    //                     diff: diff
+    //                 })
+    //             }
+    //         })
+    // }
 
     render() {
         return (
             <div >
                 <Route exact path="/">
                     <NoSsr>
-                    <Container fluid>
-                        {
+                    <div >
+                        {/* {
                             this.state.summary &&
                             <SummaryBoard input={this.state.summary} diff={this.state.diff} handleClick={this.handleClick}/>
-                        }
-                        <div className="inner_container">
+                        } */}
+                        <TempBoard input={this.state.summary} diff={this.state.diff}/>
+                        {/* <div className="inner_container">
                             <div className="data_chart">
                                 {
                                     this.state.data &&
@@ -77,15 +77,15 @@ class Page extends Component {
                                     <Map input={ this.state.data} catorgry={this.state.choosenCategory} maxColor={this.state.maxColor} minColor={this.state.minColor}/>
                                 } 
                             </div>
-                        </div>
-                    </Container>
+                        </div> */}
+                    </div>
                     </NoSsr>
                 </Route>
-                <Route path="/news">
+                {/* <Route path="/news">
                     <NoSsr>
                         <News/>
                     </NoSsr>
-                </Route>
+                </Route> */}
             </div>    
         )
     }
