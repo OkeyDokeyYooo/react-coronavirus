8bf251081f4d89f6ae745689bb478830fc3b1774
OkeyDokeyYooo <873262667a@gmail.com>
2021-02-05 16:49:23 -0800

    minor fixed

--- backend/routes/entries.js
+++ backend/routes/entries.js
@@ -1,50 +1,55 @@
-const router = require('express').Router();
-const express = require('express');
+const router = require("express").Router();
+const express = require("express");
 
-let Entry = require('../models/entry.model');
+let Entry = require("../models/entry.model");
 
 const app = express();
 
-router.route('/').get((req, res) => {       //get method
-  console.log((new Date(new Date()).getTime() - (30 * 24 * 60 * 60 * 1000)))
+router.route("/").get((req, res) => {
+    //get method
+    // console.log(new Date(new Date()).getTime() - 30 * 24 * 60 * 60 * 1000);
 
-  Entry.find({
-    "createdAt": {
-      $gte: (new Date(new Date()).getTime() - (30 * 24 * 60 * 60 * 1000))
-    }
-  })       //mongoose method to find all the users; return a promise
-    .then(entries => res.json(entries))     //return a json that we got from the database
-    .catch(err => res.status(400).json('Error: ' + err));       //error message
+    Entry.find({
+        createdAt: {
+            $gte: new Date(new Date()).getTime() - 30 * 24 * 60 * 60 * 1000,
+        },
+    }) //mongoose method to find all the users; return a promise
+        .then((entries) => res.json(entries)) //return a json that we got from the database
+        .catch((err) => res.status(400).json("Error: " + err)); //error message
 });
 
-router.route('/:_id').get((req, res) => {        
-  // Entry.find( {_id: req.params._id})
-  Entry.findById(req.params._id)
-      .then(entry => res.json(entry))
-      .catch(err => res.status(400).json('Error: ' + err));
-  });
+router.route("/:_id").get((req, res) => {
+    // Entry.find( {_id: req.params._id})
+    Entry.findOne({ _id: req.params._id })
+        .then((entry) => {
+            res.json(entry);
+        })
+        .catch((err) => res.status(400).json("Error: " + err));
+});
 
-  router.route('/:_id').delete((req, res) => {
+router.route("/:_id").delete((req, res) => {
     Entry.findByIdAndDelete(req.params._id)
-      .then(() => res.json('Entry deleted.'))
-      .catch(err => res.status(400).json('Error: ' + err));
-  });
+        .then(() => res.json("Entry deleted."))
+        .catch((err) => res.status(400).json("Error: " + err));
+});
 
-router.route('/add').post((req, res) => {       //post method
-  // const date = Date.parse(req.body.date);
-  _id = req.body._id;
-  const trk = req.body.trk;
-  const diff = req.body.diff
+router.route("/add").post((req, res) => {
+    //post method
+    // const date = Date.parse(req.body.date);
+    _id = req.body._id;
+    const trk = req.body.trk;
+    const diff = req.body.diff;
 
-  const newEntry = new Entry({
-    _id,
-    trk,
-    diff
-  });
+    const newEntry = new Entry({
+        _id,
+        trk,
+        diff,
+    });
 
-  newEntry.save()        //mongoose save method
-    .then(() => res.json('Entry added!'))
-    .catch(err => res.status(400).json('Error: ' + err));
+    newEntry
+        .save() //mongoose save method
+        .then(() => res.json("Entry added!"))
+        .catch((err) => res.status(400).json("Error: " + err));
 });
 
 // app.put('/update/:_id', (req, res, next) => {
@@ -81,7 +86,7 @@ router.route('/add').post((req, res) => {       //post method
 //   })
 // });
 
-// router.route('/:_id').get((req, res) => {        
+// router.route('/:_id').get((req, res) => {
 //   // Entry.find( {_id: req.params._id})
 //   Entry.findById(req.params._id)
 //       .then(entry => res.json(entry))
